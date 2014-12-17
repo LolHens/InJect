6b25c73e20c3aa108558f8e411b849e38880fa4b
LolHens <pierrekisters@gmail.com>
2014-12-17 17:57:51 +0100

    added some more instruction types

---
+++ src/test/scala/org/lolhens/asmpatcher/test/test1/Main.scala
@@ -0,0 +1,8 @@
+package org.lolhens.asmpatcher.test.test1
+
+/**
+ * Created by LolHens on 17.12.2014.
+ */
+object Main {
+  def main(args: Array[String]): Unit = println("abcdefg".dropRight(1))
+}
---
+++ src/test/scala/org/lolhens/asmpatcher/test/testclasses/Testclass1.java
@@ -0,0 +1,15 @@
+package org.lolhens.asmpatcher.test.testclasses;
+
+/**
+ * Created by LolHens on 17.12.2014.
+ */
+public class Testclass1 {
+    public int test1(int i1) {
+        String a = "abc";
+        Float b = new Float(1.3f);
+        double c = 24.3;
+        long d = 3;
+        if (i1 == 2000000) return 30000;
+        return 0;
+    }
+}
